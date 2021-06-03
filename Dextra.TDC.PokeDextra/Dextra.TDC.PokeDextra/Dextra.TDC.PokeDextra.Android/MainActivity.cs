4914d3dd9b7dd4078eeb123b0e3bd7ad6a23adc4
fernando.vendramin <fernando.vendramin@dextra-sw.com>
2021-06-03 19:43:06 -0300

    change configs

--- Dextra.TDC.PokeDextra/Dextra.TDC.PokeDextra/Dextra.TDC.PokeDextra.Android/MainActivity.cs
+++ Dextra.TDC.PokeDextra/Dextra.TDC.PokeDextra/Dextra.TDC.PokeDextra.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace Dextra.TDC.PokeDextra.Droid
 {
-    [Activity(Label = "Dextra.TDC.PokeDextra", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Poke Dextra", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
