9745f7c49bf73d135a155e61fbb5bcbcb2d1290a
Fagner Muniz <fagner.santos@dextra-sw.com>
2021-06-07 09:23:15 -0300

    update icon

--- Dextra.TDC.PokeDextra/Dextra.TDC.PokeDextra/Dextra.TDC.PokeDextra.Android/MainActivity.cs
+++ Dextra.TDC.PokeDextra/Dextra.TDC.PokeDextra/Dextra.TDC.PokeDextra.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace Dextra.TDC.PokeDextra.Droid
 {
-    [Activity(Label = "Poke Dextra", Icon = "@drawable/pokebola", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Poke Dextra", Icon = "@drawable/icon", RoundIcon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
